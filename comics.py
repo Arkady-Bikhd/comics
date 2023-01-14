1effc0b28330e5129eb2041c11735c78f2ee6f53
Arkadiy <capark74@mail.ru>
2023-01-14 16:21:52 +0300

    Уточнены названия переменных

--- comics.py
+++ comics.py
@@ -12,12 +12,12 @@ def main():
     load_dotenv()
     vk_access_token = environ['VK_ACCESS_TOKEN']
     vk_group_id = environ['VK_GROUP_ID']
-    actual_version_api = '5.131'
+    actual_api_version = '5.131'
     file_name = 'image.png'
     try:
         comics_caption = download_random_comics()
         try:
-            post_wall_photo(vk_access_token, vk_group_id, actual_version_api, file_name, comics_caption)
+            post_wall_photo(vk_access_token, vk_group_id, actual_api_version, file_name, comics_caption)
         except requests.exceptions.HTTPError:
             print('Ошибка опубликования комикса')
     except requests.exceptions.HTTPError:
@@ -45,13 +45,13 @@ def download_random_comics():
     return comics_caption 
 
 
-def get_upload_vk_server_url(vk_access_token, vk_group_id, version_api, file_name):
+def get_upload_vk_server_url(vk_access_token, vk_group_id, api_version, file_name):
 
     
     url = 'https://api.vk.com/method/photos.getWallUploadServer'
     payloads = {
         'access_token': vk_access_token, 
-        'v': version_api,
+        'v': api_version,
         'group_id': vk_group_id
     }
     response = requests.get(url, params=payloads)
@@ -71,10 +71,10 @@ def print_error_msg(response, file_name):
     exit() 
 
 
-def upload_wall_photos(vk_access_token, vk_group_id, version_api, file_name):
+def upload_wall_photos(vk_access_token, vk_group_id, api_version, file_name):
     
     with open(file_name, 'rb') as file:
-        url = get_upload_vk_server_url(vk_access_token, vk_group_id, version_api, file_name)
+        url = get_upload_vk_server_url(vk_access_token, vk_group_id, api_version, file_name)
         files = {
             'photo': file
         }
@@ -90,18 +90,18 @@ def upload_wall_photos(vk_access_token, vk_group_id, version_api, file_name):
         return response
                  
 
-def save_wall_photo(vk_access_token, vk_group_id,  version_api, file_name):
+def save_wall_photo(vk_access_token, vk_group_id,  api_version, file_name):
 
     url = 'https://api.vk.com/method/photos.saveWallPhoto'  
     
-    response_upload_photo = upload_wall_photos(vk_access_token, vk_group_id, version_api, file_name)    
+    upload_photo_response = upload_wall_photos(vk_access_token, vk_group_id, api_version, file_name)    
     payloads = {
         'access_token': vk_access_token,         
         'group_id': vk_group_id,
-        'photo': response_upload_photo['photo'],
-        'server': response_upload_photo['server'],
-        'v': version_api,
-        'hash': response_upload_photo['hash']
+        'photo': upload_photo_response['photo'],
+        'server': upload_photo_response['server'],
+        'v': api_version,
+        'hash': upload_photo_response['hash']
     }
     response = requests.post(url, params=payloads)
     response.raise_for_status()
@@ -112,20 +112,20 @@ def save_wall_photo(vk_access_token, vk_group_id,  version_api, file_name):
         return response['response']
 
 
-def post_wall_photo(vk_access_token, vk_group_id, version_api, file_name, caption):
+def post_wall_photo(vk_access_token, vk_group_id, api_version, file_name, caption):
 
     url = 'https://api.vk.com/method/wall.post'  
     
-    response_save_wall_photo = save_wall_photo(vk_access_token, vk_group_id, version_api, file_name)
-    owner_id = response_save_wall_photo[0]['owner_id']
-    media_id = response_save_wall_photo[0]['id']
+    save_wall_photo_response = save_wall_photo(vk_access_token, vk_group_id, api_version, file_name)
+    owner_id = save_wall_photo_response[0]['owner_id']
+    media_id = save_wall_photo_response[0]['id']
     payloads = {
         'access_token': vk_access_token,         
         'owner_id': -int(vk_group_id),
         'from_group': 1,
         'message': caption,
         'attachments': f'photo{owner_id}_{media_id}',        
-        'v': version_api,        
+        'v': api_version,        
     }
     response = requests.post(url, params=payloads)
     response.raise_for_status()
