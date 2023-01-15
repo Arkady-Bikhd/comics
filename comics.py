180ead5af9314f4ee3b99be9eb9d60b96fdb99f8
Arkadiy <capark74@mail.ru>
2023-01-15 20:11:29 +0300

    Развёрнут код

--- comics.py
+++ comics.py
@@ -76,56 +76,52 @@ def upload_comic_file(url, file_name):
 
 def upload_wall_photo(file_name, url):
     
-    if url:
-        upload_file_response = upload_comic_file(url, file_name)
-        upload_wall_photo_response = upload_file_response.json()
-        if 'error' in upload_wall_photo_response:
-            raise HTTPError(upload_wall_photo_response['error']['error_msg'])  
-        elif upload_wall_photo_response['photo'] == '[]':
-            raise HTTPError('Комикс не загружен в группу')                    
-        return upload_wall_photo_response
+    upload_file_response = upload_comic_file(url, file_name)
+    upload_wall_photo_response = upload_file_response.json()
+    if 'error' in upload_wall_photo_response:
+        raise HTTPError(upload_wall_photo_response['error']['error_msg'])  
+    elif upload_wall_photo_response['photo'] == '[]':
+        raise HTTPError('Комикс не загружен в группу')                    
+    return upload_wall_photo_response
                  
 
 def save_wall_photo(vk_access_token, vk_group_id,  api_version, upload_photo_response):
 
     url = 'https://api.vk.com/method/photos.saveWallPhoto'  
-    
-    if upload_photo_response:    
-        payloads = {
-            'access_token': vk_access_token,         
-            'group_id': vk_group_id,
-            'photo': upload_photo_response['photo'],
-            'server': upload_photo_response['server'],
-            'v': api_version,
-            'hash': upload_photo_response['hash']
+    payloads = {
+        'access_token': vk_access_token,         
+        'group_id': vk_group_id,
+        'photo': upload_photo_response['photo'],
+        'server': upload_photo_response['server'],
+        'v': api_version,
+        'hash': upload_photo_response['hash']
         }
-        response = requests.post(url, params=payloads)
-        response.raise_for_status()
-        save_wall_photo_response = response.json()    
-        if 'error' in save_wall_photo_response:
-            raise HTTPError(save_wall_photo_response['error']['error_msg']) 
-        return save_wall_photo_response['response']
+    response = requests.post(url, params=payloads)
+    response.raise_for_status()
+    save_wall_photo_response = response.json()    
+    if 'error' in save_wall_photo_response:
+        raise HTTPError(save_wall_photo_response['error']['error_msg']) 
+    return save_wall_photo_response['response']
 
 
 def post_wall_photo(vk_access_token, vk_group_id, api_version, file_name, caption, save_wall_photo_response):
 
     url = 'https://api.vk.com/method/wall.post'  
-    if save_wall_photo_response:
-        owner_id = save_wall_photo_response[0]['owner_id']
-        media_id = save_wall_photo_response[0]['id']
-        payloads = {
-            'access_token': vk_access_token,         
-            'owner_id': -int(vk_group_id),
-            'from_group': 1,
-            'message': caption,
-            'attachments': f'photo{owner_id}_{media_id}',        
-            'v': api_version,        
-        }
-        response = requests.post(url, params=payloads)
-        response.raise_for_status()
-        post_wall_photo_response = response.json()
-        if 'error' in post_wall_photo_response:
-            raise HTTPError(post_wall_photo_response['error']['error_msg']) 
+    owner_id = save_wall_photo_response[0]['owner_id']
+    media_id = save_wall_photo_response[0]['id']
+    payloads = {
+        'access_token': vk_access_token,         
+        'owner_id': -int(vk_group_id),
+        'from_group': 1,
+        'message': caption,
+        'attachments': f'photo{owner_id}_{media_id}',        
+        'v': api_version,        
+    }
+    response = requests.post(url, params=payloads)
+    response.raise_for_status()
+    post_wall_photo_response = response.json()
+    if 'error' in post_wall_photo_response:
+        raise HTTPError(post_wall_photo_response['error']['error_msg']) 
                
 
 def post_comic_in_vk(vk_access_token, vk_group_id, api_version, file_name, caption):
